bd358b71df7c6d5d03be4425a84c7633d5e8cbad
nurbekovemil <nurbekovemil@gmail.com>
2023-09-20 18:19:17 +0600

    settings config files

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,14 +4,16 @@ import * as cookieParser from 'cookie-parser';
 async function start() {
   try {
     const PORT = process.env.PORT || 3000;
-    const HOST = process.env.LOCAL_PUBLIC_SERVER_HOST;
+    const HOST = process.env.SERVER_HOST;
     const app = await NestFactory.create(AppModule);
     app.enableCors({
       origin: process.env.CLIENT_HOST,
       credentials: true,
     });
     app.use(cookieParser());
-    await app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+    await app.listen(PORT, HOST, () =>
+      console.log(`Server started on ${PORT}`),
+    );
   } catch (error) {}
 }
 
