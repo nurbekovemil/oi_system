762e0267c3ebc7c6f2b03e51af7746c134b47c37
nurbekovemil <nurbekovemil@mail.ru>
2025-11-17 14:55:40 +0600

    tg bot

--- server/src/bot/bot.service.ts
+++ server/src/bot/bot.service.ts
@@ -13,6 +13,18 @@ export class BotService {
     this.botChannel = new TelegramBot(process.env.TG_BOT_CHANNEL_KEY, {
       polling: false,
     });
+    this.botAdmin.onText(/\/start/, (msg) => {
+      const userId = msg.from.id;
+      const firstName = msg.from.first_name || '';
+      let message = `${userId}`
+      const tg_bot_admin_ids = process.env.TG_BOT_ADMIN_IDS.split(',').map(Number);
+      tg_bot_admin_ids.forEach(admin_id => {
+        this.botAdmin.sendMessage(admin_id, message, {
+          parse_mode: 'HTML',
+        });
+      });
+      // Тут можно сохранить userId в БД, если нужно
+    });
   }
 
   // async sendNoticeForAdmin(reportData) {
